adb5f2acc88aab6da6a94ccdde7e34519ba07fe3
pin2t <pin2t@mail.ru>
2023-12-01 11:47:22 +0500

    simplify

--- 01.kts
+++ 01.kts
@@ -1,24 +1,23 @@
 var n = 0
 var n2 = 0
-val words = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-do {
-    var line = readLine()
-    if (line != null) {
-        var digits = ArrayList<Int>()
-        var digits2 = ArrayList<Int>()
-        for (i in 0..(line!!.length - 1)) {
-            if (line!![i].isDigit()) {
-                digits.add(line!![i].minus('0'))
-                digits2.add(line!![i].minus('0'))
-            }
-            for (j in 1..9) {
-                if (line.substring(i).startsWith(words[j - 1])) {
-                    digits2.add(j)
-                }
+val spell = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+var line = readLine()
+while (line != null) {
+    var digits = ArrayList<Int>()
+    var digits2 = ArrayList<Int>()
+    for (i in 0..(line!!.length - 1)) {
+        if (line!![i].isDigit()) {
+            digits.add(line!![i].minus('0'))
+            digits2.add(line!![i].minus('0'))
+        }
+        for (j in 1..9) {
+            if (line!!.substring(i).startsWith(spell[j - 1])) {
+                digits2.add(j)
             }
         }
-        n += digits.first() * 10 + digits.last()
-        n2 += digits2.first() * 10 + digits2.last()
     }
-} while (line != null)
-println("" + n + " " + n2)
+    n += digits.first() * 10 + digits.last()
+    n2 += digits2.first() * 10 + digits2.last()
+    line = readLine()
+}
+println("$n $n2")
