d3922b1bab8c267e40fdb1a8444b802ef1d6d625
pin2t <pin2t@mail.ru>
2023-12-01 12:30:09 +0500

    avoid ugly !!

--- 01.kts
+++ 01.kts
@@ -1,23 +1,26 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
 var n = 0
 var n2 = 0
-val spell = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-var line = readLine()
-while (line != null) {
-    var digits = ArrayList<Int>()
-    var digits2 = ArrayList<Int>()
-    for (i in 0..(line!!.length - 1)) {
-        if (line!![i].isDigit()) {
-            digits.add(line!![i].minus('0'))
-            digits2.add(line!![i].minus('0'))
+val spell = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val scanner = Scanner(System.`in`)
+while (scanner.hasNext()) {
+    val line = scanner.nextLine()
+    val digits = ArrayList<Int>()
+    val digits2 = ArrayList<Int>()
+    for (i in line.indices) {
+        if (line[i].isDigit()) {
+            digits.add(line[i].minus('0'))
+            digits2.add(line[i].minus('0'))
         }
-        for (j in 1..9) {
-            if (line!!.substring(i).startsWith(spell[j - 1])) {
+        for (j in spell.indices) {
+            if (line.substring(i).startsWith(spell[j])) {
                 digits2.add(j)
             }
         }
     }
     n += digits.first() * 10 + digits.last()
     n2 += digits2.first() * 10 + digits2.last()
-    line = readLine()
 }
 println("$n $n2")
