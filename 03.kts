5a132fbed6c2cc3992d6ea50da56c71c6df8005d
pin2t <pin2t@mail.ru>
2023-12-03 10:48:15 +0500

    simplify

--- 03.kts
+++ 03.kts
@@ -1,5 +1,5 @@
 import java.util.*
-import kotlin.collections.ArrayList
+import java.util.Collections.singleton
 
 data class PartNumber(val start: Pair<Int, Int>, val end: Pair<Int, Int>, val value: Int)
 
@@ -10,6 +10,8 @@ val stars = HashSet<Pair<Int, Int>>()
 var y = 0
 while (scanner.hasNext()) {
     val line = scanner.nextLine()
+    line.forEachIndexed { x, c -> if (c != '.') symbols.add(Pair(x, y)) }
+    line.forEachIndexed { x, c -> if (c == '*') stars.add(Pair(x, y)) }
     var x = 0
     while (x < line.length) {
         if (line[x].isDigit()) {
@@ -17,14 +19,8 @@ while (scanner.hasNext()) {
             x++
             while (x < line.length && line[x].isDigit()) x++
             numbers.add(PartNumber(start, Pair(x - 1, y), line.substring(start.first, x).toInt()))
-            x--
-        } else if (line[x] == '*') {
-            stars.add(Pair(x, y))
-            symbols.add(Pair(x, y))
-        } else if (line[x] != '.') {
-            symbols.add(Pair(x, y))
-        }
-        x++
+        } else
+            x++
     }
     y++
 }
@@ -39,7 +35,7 @@ fun around(pos: Pair<Int, Int>, symbols: Set<Pair<Int, Int>>): Boolean {
 
 var ratios = 0
 for (star in stars) {
-    val naround = numbers.filter { around(it.start, Collections.singleton(star)) || around(it.end, Collections.singleton(star)) }.toList()
+    val naround = numbers.filter { around(it.start, singleton(star)) || around(it.end, singleton(star)) }.toList()
     if (naround.size == 2) {
         ratios += naround[0].value * naround[1].value
     }
