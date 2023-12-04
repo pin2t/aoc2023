2025db68f720da2a9cf2499fa7b634815e162daa
pin2t <pin2t@mail.ru>
2023-12-04 13:03:25 +0500

    numbers as strings

--- 04.kts
+++ 04.kts
@@ -6,10 +6,10 @@ var cards = System.`in`.bufferedReader().readLines()
 var copies = ArrayList<Int>()
 cards.forEach { copies.add(1) }
 for (i in cards.indices) {
-    val winning = number.findAll(cards[i].split("|")[0].split(":")[1]).map { it.value.toInt() }.toList()
+    val winning = number.findAll(cards[i].split("|")[0].split(":")[1]).map { it.value }
     var p = 0
     var j = 1
-    for (n in number.findAll(cards[i].split("|")[1]).map { it.value.toInt() }.toList()) {
+    for (n in number.findAll(cards[i].split("|")[1]).map { it.value }) {
         if (winning.contains(n)) {
             if (p == 0) p = 1 else p *= 2
             copies[i + (j++)] += copies[i]
