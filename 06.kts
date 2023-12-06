3f49efa5260e9d43fd4156713ef89fb597be0de4
pin2t <pin2t@mail.ru>
2023-12-06 10:36:06 +0500

    simplify

--- 06.kts
+++ 06.kts
@@ -4,16 +4,9 @@ val times = number.findAll(input[0]).map { it.value.toInt() }.toList()
 val distances = number.findAll(input[1]).map { it.value.toInt() }.toList()
 var ways = 1
 for (i in times.indices) {
-    var wins = 0
-    for (j in 0..times[i]) {
-        if (j * (times[i]-j) > distances[i]) wins++
-    }
-    ways *= wins
+    ways *= (0..times[i]).filter { (it * (times[i]-it)) > distances[i] }.count()
 }
-var wins = 0
 val distance = number.findAll(input[1].replace(" ", "")).map { it.value.toLong() }.first()
 val time = number.findAll(input[0].replace(" ", "")).map { it.value.toLong() }.first()
-for (j in 0..time) {
-    if (j * (time-j) > distance) wins++
-}
+val wins = (0..time).filter { (it * (time-it)) > distance }.count()
 println("$ways $wins")
