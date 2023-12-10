2d337ed708778761eb6e138949ba36aabaec036f
pin2t <pin2t@mail.ru>
2023-12-10 15:13:30 +0500

    day 10

--- 10.kts
+++ 10.kts
@@ -1,18 +1,19 @@
 var tiles = System.`in`.bufferedReader().readLines().map { it.toByteArray() }
+fun tile(c: Int, r: Int): Char {
+    if (r < 0 || r >= tiles.size) return ' '
+    if (c < 0 || c >= tiles[r].size) return ' '
+    return tiles[r][c].toChar()
+}
 var start = Pair(0, 0)
-for (y in tiles.indices) {
-    if (tiles[y].indexOf('S'.code.toByte()) != -1) {
-        start = Pair(tiles[y].indexOf('S'.code.toByte()), y)
+for (r in tiles.indices) {
+    val c = tiles[r].indexOf('S'.code.toByte())
+    if (c != -1) {
+        start = Pair(c, r)
         break
     }
 }
 var steps = 0
 var pos = start
-fun tile(x: Int, y: Int): Char {
-    if (y < 0 || y >= tiles.size) return ' '
-    if (x < 0 || x >= tiles[y].size) return ' '
-    return tiles[y][x].toChar()
-}
 var dir = Pair(1, 0)
 if ("-J7".contains(tile(pos.first + 1, pos.second))) dir = Pair(1, 0)
 else if ("|JL".contains(tile(pos.first, pos.second + 1))) dir = Pair(0, 1)
@@ -42,11 +43,13 @@ do {
 for (r in 0..<(tiles.size * 2)) {
     for (c in 0..<(tiles[0].size * 2)) {
         if (loop2.contains(Pair(c + 1, r)) && loop2.contains(Pair(c - 1, r)) &&
-            "L-FS".contains(tile((c - 1) / 2, r / 2)) && "J-7S".contains(tile((c + 1) / 2, r / 2)))
+            "L-FS".contains(tile((c - 1) / 2, r / 2)) && "J-7S".contains(tile((c + 1) / 2, r / 2))) {
             loop2.add(Pair(c, r))
+        }
         if (loop2.contains(Pair(c, r - 1)) && loop2.contains(Pair(c, r + 1)) &&
-            "7|FS".contains(tile(c / 2, (r - 1) / 2)) && "J|LS".contains(tile(c / 2, (r + 1) / 2)))
+            "7|FS".contains(tile(c / 2, (r - 1) / 2)) && "J|LS".contains(tile(c / 2, (r + 1) / 2))) {
             loop2.add(Pair(c, r))
+        }
     }
 }
 fun inner(loop: Set<Pair<Int, Int>>, pos: Pair<Int, Int>): Boolean {
@@ -67,7 +70,11 @@ fun inner(loop: Set<Pair<Int, Int>>, pos: Pair<Int, Int>): Boolean {
     return true
 }
 var inners = 0
-for (r in tiles.indices) for (c in tiles[r].indices)
-    if (!loop.contains(Pair(c, r)) && inner(loop2, Pair(c * 2, r * 2)))
-        inners++
+for (r in tiles.indices) {
+    for (c in tiles[r].indices) {
+        if (!loop.contains(Pair(c, r)) && inner(loop2, Pair(c * 2, r * 2))) {
+            inners++
+        }
+    }
+}
 println(listOf(steps / 2, inners))
