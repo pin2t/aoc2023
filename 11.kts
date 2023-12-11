f44accb5fb178917ed1d964d84ee4c3a661ee044
pin2t <pin2t@mail.ru>
2023-12-11 10:52:10 +0500

    day 11

--- 11.kts
+++ 11.kts
@@ -1,46 +1,36 @@
 import java.util.*
 import kotlin.math.abs
 
-data class Galaxy(var col: Long, var row: Long)
+data class Galaxy(var col: Long, var row: Long) {
+    fun distance(other: Galaxy): Long = abs(this.row - other.row) + abs(this.col - other.col)
+}
 val scanner = Scanner(System.`in`)
 val galaxies = ArrayList<Galaxy>()
-val galaxies2 = ArrayList<Galaxy>()
 var rows: Long = 0
-var cols: Long = 0
 while (scanner.hasNext()) {
-    val line = scanner.nextLine()
-    line.forEachIndexed { index, c ->
-        if (c == '#') {
-            galaxies.add(Galaxy(index.toLong(), rows))
-            galaxies2.add(Galaxy(index.toLong(), rows))
-        }
-    }
-    cols = line.length.toLong()
+    scanner.nextLine().forEachIndexed { index, c -> if (c == '#') galaxies.add(Galaxy(index.toLong(), rows)) }
     rows++
 }
-var r = rows - 1
-while (r >= 0) {
+var cols: Long = galaxies.maxOf { it.col }
+val galaxies2 = galaxies.map { it.copy() }
+for (r in rows downTo 0) {
     if (galaxies.all { it.row != r }) {
         galaxies.forEach { if (it.row > r) it.row++ }
         galaxies2.forEach { if (it.row > r) it.row+=999999 }
-        rows++
     }
-    r--
 }
-var c  = cols - 1
-while (c >= 0) {
+for (c in cols downTo 0) {
     if (galaxies.all { it.col != c }) {
         galaxies.forEach { if (it.col > c) it.col++ }
         galaxies2.forEach { if (it.col > c) it.col+=999999 }
-        cols++
     }
-    c--
 }
 var sum: Long = 0
 var sum2: Long = 0
-for (i in 0..<(galaxies.size - 1)) for (j in i..<galaxies.size) {
-    val s = abs(galaxies[i].row - galaxies[j].row) + abs(galaxies[i].col - galaxies[j].col)
-    sum += s
-    sum2 += abs(galaxies2[i].row - galaxies2[j].row) + abs(galaxies2[i].col - galaxies2[j].col)
+for (i in 0..<(galaxies.size - 1)) {
+    for (j in i..<galaxies.size) {
+        sum += galaxies[i].distance(galaxies[j])
+        sum2 += galaxies2[i].distance(galaxies2[j])
+    }
 }
 println(listOf(sum, sum2))
