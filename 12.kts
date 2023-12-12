36790731d004054f3287cbfd8c634a263dc8ab9c
pin2t <pin2t@mail.ru>
2023-12-12 12:03:46 +0500

    day 12

--- 12.kts
+++ 12.kts
@@ -5,47 +5,42 @@ var sum: Long = 0; var sum2: Long = 0
 while (scanner.hasNext()) {
     val line = scanner.nextLine()
     var springs = line.split(' ')[0]
-    var sizes = ArrayList<Int>(line.split(' ')[1].split(',').map { it.toInt() }.toList())
-    var matchCount: Long = 0
+    val sizes = ArrayList(line.split(' ')[1].split(',').map { it.toInt() }.toList())
+    var matches: Long = 0
     val cache = HashMap<Pair<Int, Int>, Long>()
-    fun matchAll(a: String, i: Int) {
-        if (a.length <= springs.length && cache.contains(Pair(a.length, i))) {
-            matchCount += cache.get(Pair(a.length, i))!!
+    fun matched(prefix: String): Boolean {
+        var result = true
+        prefix.forEachIndexed { index, c -> result = result && (c == springs[index] || springs[index] == '?') }
+        return result
+    }
+    fun match(prefix: String, group: Int) {
+        if (prefix.length <= springs.length && cache.contains(Pair(prefix.length, group))) {
+            matches += cache[Pair(prefix.length, group)]!!
             return
         }
-        if (i == sizes.size) {
-            if (a.length <= springs.length) {
-                var matched = true
-                (a + ".".repeat(springs.length - a.length)).forEachIndexed { index, c ->
-                    matched = matched && (c == springs[index] || springs[index] == '?')
-                }
-                if (matched) { matchCount++ }
-            }
+        if (group == sizes.size) {
+            if (prefix.length <= springs.length && matched(prefix + ".".repeat(springs.length - prefix.length))) { matches++ }
             return
         }
-        for (ii in 0..(springs.length - sizes[i] - 1)) {
-            var aa = a;
-            if (i > 0) aa += "."
-            aa += ".".repeat(ii) + "#".repeat(sizes[i])
-            if (aa.length <= springs.length) {
-                var matched = true
-                aa.forEachIndexed { index, c -> matched = matched && (c == springs[index] || springs[index] == '?') }
-                if (matched) {
-                    var prev = matchCount
-                    matchAll(aa, i + 1)
-                    cache.put(Pair(aa.length, i + 1), matchCount - prev)
-                }
+        for (i in 0..<(springs.length - sizes[group])) {
+            var p = prefix;
+            if (group > 0) p += "."
+            p += ".".repeat(i) + "#".repeat(sizes[group])
+            if (p.length <= springs.length && matched(p)) {
+                val prev = matches
+                match(p, group + 1)
+                cache[Pair(p.length, group + 1)] = matches - prev
             }
         }
     }
-    matchAll("", 0)
-    sum += matchCount
-    matchCount = 0
+    match("", 0)
+    sum += matches
+    matches = 0
     cache.clear()
     val sizes2 = ArrayList<Int>(sizes);
     sizes.addAll(sizes2); sizes.addAll(sizes2); sizes.addAll(sizes2); sizes.addAll(sizes2)
     springs = "$springs?$springs?$springs?$springs?$springs"
-    matchAll("", 0)
-    sum2 += matchCount
+    match("", 0)
+    sum2 += matches
 }
 println(listOf(sum, sum2))
