0703b7a29cf9b32abf0beafad9e83332bc4f2193
pin2t <pin2t@mail.ru>
2023-12-12 13:20:57 +0500

    day 12

--- 12.kts
+++ 12.kts
@@ -4,29 +4,24 @@ var springs: String = ""
 var sizes: ArrayList<Int> = ArrayList<Int>()
 val cache = HashMap<Pair<Int, Int>, Long>()
 fun matched(prefix: String): Boolean {
-    var result = true
-    prefix.forEachIndexed { index, c -> result = result && (c == springs[index] || springs[index] == '?') }
-    return result
+    return prefix.filterIndexed { index, c -> c == springs[index] || springs[index] == '?' }.count() == prefix.length
 }
 fun matches(prefix: String, group: Int): Long {
-    if (prefix.length <= springs.length && cache.contains(Pair(prefix.length, group))) {
+    if (prefix.length > springs.length) return 0
+    if (cache.contains(Pair(prefix.length, group))) {
         return cache[Pair(prefix.length, group)]!!
     }
     if (group == sizes.size) {
-        if (prefix.length <= springs.length && matched(prefix + ".".repeat(springs.length - prefix.length))) return 1
-        return 0
+        return if (matched(prefix + ".".repeat(springs.length - prefix.length))) 1 else 0
     }
     var result: Long = 0
     for (i in 0..<(springs.length - sizes[group])) {
         var p = prefix;
         if (group > 0) p += "."
         p += ".".repeat(i) + "#".repeat(sizes[group])
-        if (p.length <= springs.length && matched(p)) {
-            var m = matches(p, group + 1)
-            cache[Pair(p.length, group + 1)] = m
-            result += m
-        }
+        if (p.length <= springs.length && matched(p)) result += matches(p, group + 1)
     }
+    cache[Pair(prefix.length, group)] = result
     return result
 }
 var sum: Long = 0; var sum2: Long = 0
