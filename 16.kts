52611258f1429ae2737b1ea51c2f93e4bc889a16
pin2t <pin2t@mail.ru>
2023-12-16 11:15:34 +0500

    day 16

--- 16.kts
+++ 16.kts
@@ -1,65 +1,60 @@
 import kotlin.math.max
 
-var grid = System.`in`.bufferedReader().lines().toList()
-data class Beam(val pos: Pair<Int, Int>, val dir: Pair<Int, Int>)
+val grid = System.`in`.bufferedReader().lines().toList()
 val up = Pair(0, -1); val right = Pair(1, 0); val down = Pair(0, 1); val left = Pair(-1, 0)
-fun energized(b: Beam): Int {
-    val queue = ArrayDeque<Beam>()
-    val processed = HashSet<Beam>()
-    val energized = HashSet<Pair<Int, Int>>()
-    queue.add(b)
-    while (!queue.isEmpty()) {
-        val beam = queue.removeFirst()
-        if (beam.pos.first < 0 || beam.pos.second < 0 || beam.pos.first >= grid[0].length || beam.pos.second >= grid.size ||
-            processed.contains(beam))
-            continue
-        energized.add(beam.pos)
-        processed.add(beam)
-        when (grid[beam.pos.second][beam.pos.first]) {
-            '-' -> {
-                if (beam.dir == left || beam.dir == right)
-                    queue.add(Beam(pos = Pair(beam.pos.first + beam.dir.first, beam.pos.second + beam.dir.second), dir = beam.dir))
-                else {
-                    queue.add(Beam(pos = Pair(beam.pos.first - 1, beam.pos.second), dir = left))
-                    queue.add(Beam(pos = Pair(beam.pos.first + 1, beam.pos.second), dir = right))
+data class Beam(val pos: Pair<Int, Int>, val dir: Pair<Int, Int>) {
+    private fun inside() = pos.first >= 0 && pos.second >= 0 && pos.first < grid[0].length && pos.second < grid.size
+    private fun move(): List<Beam> {
+        when (grid[pos.second][pos.first]) {
+            '-' -> 
+                if (dir == left || dir == right) 
+                    return listOf(Beam(Pair(pos.first + dir.first, pos.second + dir.second), dir))
+                else 
+                    return listOf(Beam(Pair(pos.first - 1, pos.second), left), Beam(Pair(pos.first + 1, pos.second), right))
+            '|' ->
+                if (dir.first == 0)
+                    return listOf(Beam(Pair(pos.first + dir.first, pos.second + dir.second), dir))
+                else
+                    return listOf(Beam(Pair(pos.first, pos.second - 1), Pair(0, -1)), Beam(Pair(pos.first, pos.second + 1), Pair(0, 1)))
+            '/' ->
+                when (dir) {
+                    up -> return listOf(Beam(Pair(pos.first + 1, pos.second), right))
+                    right -> return listOf(Beam(Pair(pos.first, pos.second - 1), up))
+                    down -> return listOf(Beam(Pair(pos.first - 1, pos.second), left))
+                    left -> return listOf(Beam(Pair(pos.first, pos.second + 1), down))
                 }
-            }
-            '|' -> {
-                if (beam.dir.first == 0)
-                    queue.add(Beam(pos = Pair(beam.pos.first + beam.dir.first, beam.pos.second + beam.dir.second), dir = beam.dir))
-                else {
-                    queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second - 1), dir = Pair(0, -1)))
-                    queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second + 1), dir = Pair(0, 1)))
+            '\\' ->
+                when (dir) {
+                    up -> return listOf(Beam(Pair(pos.first - 1, pos.second), left))
+                    right -> return listOf(Beam(Pair(pos.first, pos.second + 1), down))
+                    down -> return listOf(Beam(Pair(pos.first + 1, pos.second), right))
+                    left -> return listOf(Beam(Pair(pos.first, pos.second - 1), up))
                 }
-            }
-            '/' -> {
-                when (beam.dir) {
-                    up -> queue.add(Beam(pos = Pair(beam.pos.first + 1, beam.pos.second), dir = right))
-                    right -> queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second - 1), dir = up))
-                    down -> queue.add(Beam(pos = Pair(beam.pos.first - 1, beam.pos.second), dir = left))
-                    left -> queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second + 1), dir = down))
-                }
-            }
-            '\\' -> {
-                when (beam.dir) {
-                    up -> queue.add(Beam(pos = Pair(beam.pos.first - 1, beam.pos.second), dir = left))
-                    right -> queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second + 1), dir = down))
-                    down -> queue.add(Beam(pos = Pair(beam.pos.first + 1, beam.pos.second), dir = right))
-                    left -> queue.add(Beam(pos = Pair(beam.pos.first, beam.pos.second - 1), dir = up))
-                }
-            }
-            '.' -> queue.add(Beam(pos = Pair(beam.pos.first + beam.dir.first, beam.pos.second + beam.dir.second), dir = beam.dir))
+            '.' -> return listOf(Beam(Pair(pos.first + dir.first, pos.second + dir.second), dir))
         }
+        throw IllegalStateException()
     }
-    return energized.size
+    fun energized(): Int {
+        val queue = ArrayDeque<Beam>()
+        val processed = HashSet<Beam>()
+        val energized = HashSet<Pair<Int, Int>>()
+        queue.add(this)
+        while (!queue.isEmpty()) {
+            val beam = queue.removeFirst()
+            if (!beam.inside() || processed.contains(beam))
+                continue
+            energized.add(beam.pos)
+            processed.add(beam)
+            queue.addAll(beam.move())
+        }
+        return energized.size
+    }
 }
 var result2 = 0
 for (r in 0..<grid.size) {
-    result2 = max(result2,
-        max(energized(Beam(pos = Pair(0, r), dir = right)), energized(Beam(pos = Pair(grid[0].length - 1, r), dir = left))))
+    result2 = max(result2, max(Beam(Pair(0, r), right).energized(), Beam(Pair(grid[0].length - 1, r), left).energized()))
 }
 for (c in 0..<grid[0].length) {
-    result2 = max(result2,
-        max(energized(Beam(pos = Pair(c, 0), dir = down)), energized(Beam(pos = Pair(c, grid.size - 1), dir = up))))
+    result2 = max(result2, max(Beam(Pair(c, 0), down).energized(), Beam(Pair(c, grid.size - 1), up).energized()))
 }
-println(listOf(energized(Beam(pos = Pair(1, 0), dir = right)), result2))
+println(listOf(Beam(Pair(1, 0), right).energized(), result2))
