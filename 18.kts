5941bbffa02e8c46f1c82c004328b8e4342fbc6b
pin2t <pin2t@mail.ru>
2023-12-18 10:19:08 +0400

    day 18

--- 18.kts
+++ 18.kts
@@ -14,7 +14,9 @@ fun dig(dir: Pair<Int, Int>, n: Int) {
     }
 }
 fun move(pos: Pair<Int, Int>, dir: Pair<Int, Int>): Pair<Int, Int> = Pair(pos.first + dir.first, pos.second + dir.second)
-var loop2 = 1
+fun move(pos: Pair<Int, Int>, dir: Pair<Int, Int>, n: Int): Pair<Int, Int> =
+    Pair(pos.first + dir.first * n, pos.second + dir.second * n)
+var loop2: Long = 1
 while (scanner.hasNext()) {
     val line = scanner.nextLine().split(' ')
     val n = line[1].toInt()
@@ -23,10 +25,10 @@ while (scanner.hasNext()) {
     }
     val n2 = line[2].substring(2, 7).toInt(16)
     when (line[0][0]) {
-        'U' -> for (i in 1..n2) pos2 = move(pos2, up)
-        'D' -> for (i in 1..n2) pos2 = move(pos2, down)
-        'L' -> for (i in 1..n2) pos2 = move(pos2, left)
-        'R' -> for (i in 1..n2) pos2 = move(pos2, right)
+        'U' -> pos2 = move(pos2, up, n2)
+        'D' -> pos2 = move(pos2, down, n2)
+        'L' -> pos2 = move(pos2, left, n2)
+        'R' -> pos2 = move(pos2, right, n2)
     }
     loop2 += n2
     corners.add(pos2)
@@ -34,7 +36,7 @@ while (scanner.hasNext()) {
 corners.add(Pair(0, 0))
 val l = loop.minOf { it.first }; val r = loop.maxOf { it.first }
 val t = loop.minOf { it.second }; val b = loop.maxOf { it.second }
-fun insideCount(pos: Pair<Int, Int>): Int {
+fun inside(pos: Pair<Int, Int>): Int {
     val processed = HashSet<Pair<Int, Int>>()
     val queue = LinkedList<Pair<Int, Int>>()
     queue.add(pos)
@@ -47,17 +49,20 @@ fun insideCount(pos: Pair<Int, Int>): Int {
     }
     return processed.size
 }
-var res1 = 0
-for (r in t..b) {
-    for (c in l..r) {
-        val count = insideCount(Pair(c, r))
-        if (count != 0) { res1 = loop.size + count; break }
+fun res1(): Int {
+    for (r in t..b) {
+        for (c in l..r) {
+            val count = inside(Pair(c, r))
+            if (count != 0) return loop.size + count
+        }
     }
-    if (res1 != 0) break
+    return 0
 }
-var res2: Long = 0
-for (i in 0..<(corners.size - 1))
-    res2 += corners[i].first.toLong() * corners[i + 1].second.toLong() -
-            corners[i + 1].first.toLong() * corners[i].second.toLong()
-res2 = res2 / 2 + loop2 / 2
-println(listOf(res1, res2))
+fun res2(): Long {
+    var result: Long = 0
+    for (i in 0..<(corners.size - 1))
+        result += corners[i].first.toLong() * corners[i + 1].second.toLong() -
+                  corners[i + 1].first.toLong() * corners[i].second.toLong()
+    return result / 2 + loop2 / 2
+}
+println(listOf(res1(), res2()))
