97300ea45d8cd4bd08f4818abbe0e93c8e9f013f
pin2t <pin2t@mail.ru>
2023-12-18 12:42:16 +0400

    day 17 simplify

--- 17.kts
+++ 17.kts
@@ -2,35 +2,52 @@ import java.util.*
 
 val grid = System.`in`.bufferedReader().lines().map { it.map { (it - '0') }.toList() }.toList()
 val up = Pair(0, -1); val right = Pair(1, 0); val down = Pair(0, 1); val left = Pair(-1, 0)
-data class State(val pos: Pair<Int, Int>, val dir: Pair<Int, Int>,
-                 val loss: Int, val straight: Int, val path: ArrayList<Pair<Int, Int>>)
-val queue = LinkedList<State>()
-queue.add(State(Pair(0, 0), right, 0, 1, ArrayList()))
-val losses = HashMap<Pair<Int, Int>, Pair<Int, List<Pair<Int, Int>>>>()
-losses[Pair(0, 0)] = Pair(0, ArrayList())
+data class Crucible(val pos: Pair<Int, Int>, val dir: Pair<Int, Int>, val loss: Int, val straights: Int)
+var res1 = 0; var res2 = 0
+val queue = PriorityQueue<Crucible>(compareBy { it.loss })
+queue.add(Crucible(Pair(0, 0), right, 0, 0))
+val processed = HashSet<Triple<Pair<Int, Int>, Pair<Int, Int>, Int>>()
 while (!queue.isEmpty()) {
-    val s = queue.removeFirst()
-    fun move(dir: Pair<Int, Int>) {
-        val np = Pair(s.pos.first + dir.first, s.pos.second + dir.second)
-        if (!(np.first >= 0 && np.second >= 0 && np.first < grid[0].size && np.second < grid.size)) return
-        val next = State(np, dir, s.loss + grid[np.second][np.first], s.straight + 1, ArrayList(s.path))
-        next.path.add(np)
-        if (losses.contains(next.pos) && next.loss >= losses[next.pos]!!.first) return
-        if (s.dir == dir) {
-            if (next.straight <= 3) {
-                losses[np] = Pair(next.loss, next.path)
-                queue.add(next)
-            }
-        } else {
-            if (s.dir == up && dir != down || s.dir == right && dir != left ||
-                s.dir == down && dir != up || s.dir == left && dir != right) {
-                val nn = State(np, dir, next.loss, 0, next.path)
-                losses[np] = Pair(next.loss, next.path)
-                queue.add(nn)
+    val c = queue.poll()
+    if (c.pos == Pair(grid[0].size - 1, grid.size - 1)) { res1 = c.loss; break }
+    val key = Triple(c.pos, c.dir, c.straights)
+    if (processed.contains(key)) continue
+    processed.add(key)
+    listOf(up, right, down, left).forEach { dir ->
+        val np = Pair(c.pos.first + dir.first, c.pos.second + dir.second)
+        if (np.first >= 0 && np.second >= 0 && np.first < grid[0].size && np.second < grid.size) {
+            val next = Crucible(np, dir, c.loss + grid[np.second][np.first], c.straights + 1)
+            if (c.dir == dir) {
+                if (next.straights <= 3) queue.add(next)
+            } else {
+                if (c.dir == up && dir != down || c.dir == right && dir != left || c.dir == down && dir != up || c.dir == left && dir != right) {
+                    queue.add(Crucible(next.pos, next.dir, next.loss, 1))
+                }
             }
         }
     }
-    move(up); move(right); move(down); move(left)
 }
-var res = losses.get(Pair(grid[0].size - 1, grid.size - 1))!!
-println(res.first)
+queue.clear()
+queue.add(Crucible(Pair(0, 0), right, 0, 0))
+processed.clear()
+while (!queue.isEmpty()) {
+    val c = queue.poll()
+    if (c.pos == Pair(grid[0].size - 1, grid.size - 1)) { res2 = c.loss; break }
+    val key = Triple(c.pos, c.dir, c.straights)
+    if (processed.contains(key)) continue
+    processed.add(key)
+    listOf(up, right, down, left).forEach { dir ->
+        val np = Pair(c.pos.first + dir.first, c.pos.second + dir.second)
+        if (np.first >= 0 && np.second >= 0 && np.first < grid[0].size && np.second < grid.size) {
+            val next = Crucible(np, dir, c.loss + grid[np.second][np.first], c.straights + 1)
+            if (c.dir == dir) {
+                if (next.straights <= 10) queue.add(next)
+            } else if (c.straights >= 4) {
+                if (c.dir == up && dir != down || c.dir == right && dir != left || c.dir == down && dir != up || c.dir == left && dir != right) {
+                    queue.add(Crucible(next.pos, next.dir, next.loss, 1))
+                }
+            }
+        }
+    }
+}
+println(listOf(res1, res2))
