d9a56fda837ff51b55accedfcdb13465d6a09aa3
pin2t <pin2t@mail.ru>
2023-12-20 16:56:44 +0400

    day 20 go

--- 20.go
+++ 20.go
@@ -1,31 +1,35 @@
 package main
 
-import "fmt"
 import "os"
+import "bufio"
 import (
 
-"bufio"
 "strings"
+"fmt"
 )
 
-type module struct {
+type module interface {
+	process(p pulse)
+}
+
+type broadcaster struct {
 	name string
 	outputs []string
 }
 
-func (m *module) process(high bool) {
+func (m *broadcaster) process(p pulse) {
 	for _, o := range m.outputs {
-		pulse{m.name, o, high}.send()
+		pulse{m.name, o, p.high}.send()
 	}
 }
 
 type flip struct {
-	module
+	broadcaster
 	on bool
 }
 
-func (m *flip) process(high bool) {
-	if !high {
+func (m *flip) process(p pulse) {
+	if !p.high {
 		m.on = !m.on
 		for _, o := range m.outputs {
 			pulse{m.name, o, m.on}.send()
@@ -33,13 +37,17 @@ func (m *flip) process(high bool) {
 	}
 }
 
+func (m* flip) reset() {
+	m.on = false
+}
+
 type conjunction struct {
-	module
+	broadcaster
 	pulses map[string]bool
 }
 
-func (m *conjunction) process(from string, high bool) {
-	m.pulses[from] = high
+func (m *conjunction) process(p pulse) {
+	m.pulses[p.from] = p.high
 	var result = true
 	for _, p := range m.pulses {
 		result = result && p
@@ -49,47 +57,92 @@ func (m *conjunction) process(from string, high bool) {
 	}
 }
 
+func (m* conjunction) reset() {
+	for from := range m.pulses { m.pulses[from] = false }
+}
+
 var modules = make(map[string]interface{})
-var sentHIgh, sentLow = int64(0), int64(0)
+var sentHigh, sentLow = int64(0), int64(0)
 var pulses []pulse
 
 type pulse struct {
-	from, to string;
+	from, to string
 	high bool
 }
 
 func (p pulse) send() {
 	pulses = append(pulses, p)
-	if p.high { sentHIgh++ } else { sentLow++ }
+	if p.high { sentHigh++ } else { sentLow++ }
+}
+
+func gcd(a int64, b int64) int64 {
+    if b == 0 { return a }
+    return gcd(b, a % b)
+}
+
+func lcm(a int64, b int64) int64 {
+    return a / gcd(a, b) * b
 }
 
 func main() {
 	var scanner = bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		var items = strings.Split(scanner.Text(), " -> ")
+		var name = items[0][1:]
+		var outputs = strings.Split(items[1], ",")
+		for i := range outputs {
+			outputs[i] = strings.Trim(outputs[i], " ")
+		}
 		if items[0] == "broadcaster" {
-			modules[items[0]] = &module{items[0], strings.Split(items[1], ",")}
+			modules[items[0]] = &broadcaster{items[0], outputs}
 		} else if items[0][0] == '%' {
-        	modules[items[0][1:]] = &flip{module{items[0], strings.Split(items[1], ",")}, false}
+        	modules[name] = &flip{broadcaster{name, outputs}, false}
 		} else if items[0][0] == '&' {
-			modules[items[0][1:]] = &conjunction{module{items[0], strings.Split(items[1], ",")}, make(map[string]bool)}
+			modules[name] = &conjunction{broadcaster{name, outputs},
+				make(map[string]bool)}
    		}
 	}
-	for i := 1; i <= 4000; i++ {
+	for i := 1; i <= 1000; i++ {
 		pulse{"button", "broadcaster", false}.send()
 		for len(pulses) > 0 {
 			var p = pulses[0]
 			pulses = pulses[1:]
 			if mp, found := modules[p.to]; found {
-				if m, ok := mp.(*module); ok {
-					m.process(p.high)
-				} else if m, ok := mp.(*flip); ok {
-				    m.process(p.high)
-				} else if m, ok := mp.(*conjunction); ok {
-				    m.process(p.from, p.high)
-				}
+				if m, ok := mp.(*broadcaster); ok { m.process(p) }
+				if m, ok := mp.(*flip); ok { m.process(p) }
+				if m, ok := mp.(*conjunction); ok { m.process(p) }
 			}
 		}
 	}
-	fmt.Println(sentHIgh * sentLow)
+	for _, mi := range modules {
+		if m, ok := mi.(*conjunction); ok { m.reset() }
+		if m, ok := mi.(*flip); ok { m.reset() }
+	}
+	var cycles = make(map[string]int64)
+	var press = 1
+	var target = modules["zg"].(*conjunction)
+	for {
+		pulse{"button", "broadcaster", false}.send()
+		for len(pulses) > 0 {
+			var p = pulses[0]
+			pulses = pulses[1:]
+			if _, found := target.pulses[p.to]; found {
+				if _, found := cycles[p.to]; !found && p.high {
+					cycles[p.to] = int64(press)
+				}
+				if len(cycles) == len(target.pulses) {
+					var result2 = int64(1)
+					for _, c := range cycles { result2 = lcm(result2, c) }
+					fmt.Println(sentHigh * sentLow, result2)
+					return
+				}
+			}
+			if mp, found := modules[p.to]; found {
+				if m, ok := mp.(*broadcaster); ok { m.process(p) }
+				if m, ok := mp.(*flip); ok { m.process(p) }
+				if m, ok := mp.(*conjunction); ok { m.process(p) }
+			}
+		}
+		press++
+	}
 }
