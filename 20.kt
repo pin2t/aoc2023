ae4683a6eb2afb85269a503ef4a740e9e2df7a49
pin2t <pin2t@mail.ru>
2023-12-20 11:50:29 +0400

    day 20 simplify

--- 20.kt
+++ 20.kt
@@ -23,31 +23,44 @@ fun main() {
             if (modules.contains("&" + m.value[i])) m.value[i] = "&" + m.value[i]
         }
     }
-    for (m in modules) if (m.key.startsWith('&')) conjunctions[m.key] = HashMap()
-    for (m in modules) for (o in m.value) if (conjunctions.contains(o)) conjunctions[o]!![m.key] = false
+    for (m in modules) {
+        if (m.key.startsWith('&')) conjunctions[m.key] = HashMap()
+    }
+    for (m in modules) {
+        for (o in m.value) {
+            if (conjunctions.contains(o)) conjunctions[o]!![m.key] = false
+        }
+    }
     val pulses = ArrayDeque<Pair<Boolean, String>>()
-    val sent = HashMap<Boolean, Long>(); sent[false] = 0; sent[true] = 0
+    val sent = HashMap<Boolean, Long>(); 
+    sent[false] = 0; sent[true] = 0
     fun send(from: String, to: String, value: Boolean) {
         if (conjunctions.contains(to)) conjunctions[to]!![from] = value
         pulses.add(Pair(value, to))
         sent[value] = sent[value]!! + 1
     }
     for (i in 1..1000) {
-        pulses.clear()
         send("button", "broadcaster", false)
         while (pulses.isNotEmpty()) {
             var pulse = pulses.removeFirst()
-            if (!modules.contains(pulse.second)) continue
-            if (pulse.second == "broadcaster") {
-                for (output in modules[pulse.second]!!) send(pulse.second, output, pulse.first)
-            } else if (pulse.second.startsWith('%')) {
-                if (!pulse.first) {
-                    on[pulse.second] = !on[pulse.second]!!
-                    for (output in modules[pulse.second]!!) send(pulse.second, output, on[pulse.second]!!)
+            val value = pulse.first
+            val to = pulse.second
+            if (!modules.contains(to)) continue
+            if (to == "broadcaster") {
+                for (output in modules[to]!!) {
+                    send(to, output, value)
                 }
-            } else if (pulse.second.startsWith('&')) {
-                for (output in modules[pulse.second]!!)
-                    send(pulse.second, output, !conjunctions[pulse.second]!!.all { it.value })
+            } else if (to.startsWith('%')) {
+                if (!value) {
+                    on[to] = !on[to]!!
+                    for (output in modules[to]!!) {
+                        send(to, output, on[to]!!)
+                    }
+                }
+            } else if (to.startsWith('&')) {
+                for (output in modules[to]!!) {
+                    send(to, output, !conjunctions[to]!!.all { it.value })
+                }
             }
         }
     }
@@ -57,29 +70,36 @@ fun main() {
     val cycles = HashMap<String, Long>()
     var press = 1
     while (true) {
-        pulses.clear()
         send("button", "broadcaster", false)
         while (pulses.isNotEmpty()) {
             val pulse = pulses.removeFirst()
-            if (conjunctions["&zg"]!!.contains(pulse.second)) {
-                if (!cycles.contains(pulse.second) && conjunctions["&zg"]!![pulse.second]!!)
-                    cycles[pulse.second] = press.toLong()
+            val to = pulse.second
+            val value = pulse.first
+            if (conjunctions["&zg"]!!.contains(to)) {
+                if (!cycles.contains(to) && conjunctions["&zg"]!![to]!!) {
+                    cycles[to] = press.toLong()
+                }
                 if (cycles.keys == conjunctions["&zg"]!!.keys) {
-                    println(listOf(res1, cycles.values.reduce { acc, l -> lcm(acc, l) }))
+                    println(listOf(res1, cycles.values.reduce { acc, c -> lcm(acc, c) }))
                     return
                 }
             }
-            if (!modules.contains(pulse.second)) continue
-            if (pulse.second == "broadcaster") {
-                for (output in modules[pulse.second]!!) send(pulse.second, output, pulse.first)
-            } else if (pulse.second.startsWith('%')) {
-                if (!pulse.first) {
-                    on[pulse.second] = !on[pulse.second]!!
-                    for (output in modules[pulse.second]!!) send(pulse.second, output, on[pulse.second]!!)
+            if (!modules.contains(to)) continue
+            if (to == "broadcaster") {
+                for (output in modules[to]!!) {
+                    send(to, output, value)
                 }
-            } else if (pulse.second.startsWith('&')) {
-                for (output in modules[pulse.second]!!)
-                    send(pulse.second, output, !conjunctions[pulse.second]!!.all { it.value })
+            } else if (to[0] == '%') {
+                if (!value) {
+                    on[to] = !on[to]!!
+                    for (output in modules[to]!!) {
+                        send(to, output, on[to]!!)
+                    }
+                }
+            } else if (to[0] == '&') {
+                for (output in modules[to]!!) {
+                    send(to, output, !conjunctions[to]!!.all { it.value })
+                }
             }
         }
         press++
