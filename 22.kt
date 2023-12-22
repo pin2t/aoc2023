9228b60257d8bc2c6cbe01746fd25ec145668967
pin2t <pin2t@mail.ru>
2023-12-22 13:55:31 +0400

    day 22 simplify

--- 22.kt
+++ 22.kt
@@ -1,34 +1,18 @@
+import kotlin.math.max
+import kotlin.math.min
+
 val bricks = ArrayList<Brick>()
 
 data class Brick (val s: Triple<Int, Int, Int>, val e: Triple<Int, Int, Int>) {
-    fun contains(p: Triple<Int, Int, Int>): Boolean {
-        return p.first >= s.first && p.first <= e.first &&
-                p.second >= s.second && p.second <= e.second &&
-                p.third >= s.third && p.third <= e.third
-    }
-
     fun intersect(other: Brick): Boolean {
-        if (other.s.third > e.third || other.e.third < s.third) return false
-        for (x in other.s.first..other.e.first) {
-            for (y in other.s.second..other.e.second) {
-                for (z in other.s.third..other.e.third) {
-                    if (contains(Triple(x, y, z))) return true
-                }
-            }
-        }
-        return false
+        return max(s.first, other.s.first) <= min(e.first, other.e.first) &&
+                max(s.second, other.s.second) <= min(e.second, other.e.second) &&
+                max(s.third, other.s.third) <= min(e.third, other.e.third)
     }
 
-    fun canFall(bricks: ArrayList<Brick>): Boolean {
-        val f = fallen
-        for (b in bricks) {
-            if (b != this && b.intersect(f)) return false
-        }
-        return s.third > 1
-    }
-
+    fun canFall(bricks: ArrayList<Brick>): Boolean = bricks.all { it == this || !it.intersect(fall) } && s.third > 1
     val canFall: Boolean get() = canFall(bricks)
-    val fallen: Brick get() = Brick(Triple(s.first, s.second, s.third - 1), Triple(e.first, e.second, e.third - 1))
+    val fall: Brick get() = Brick(Triple(s.first, s.second, s.third - 1), Triple(e.first, e.second, e.third - 1))
 }
 
 val number = Regex("-?\\d+")
@@ -42,20 +26,22 @@ fun main() {
     }
     bricks.sortBy { it.s.third }
     for (i in 0..<bricks.size) {
-        while (bricks[i].canFall) bricks[i] = bricks[i].fallen
+        while (bricks[i].canFall) bricks[i] = bricks[i].fall
     }
     var result1 = 0; var result2 = 0
     for (i in 0..<bricks.size) {
         val save = bricks[i]
         bricks[i] = Brick(Triple(-1, -1, -1), Triple(-1, -1, -1))
-        if (bricks.all { !it.canFall }) {
+        if (i == bricks.size - 1) {
+            result1++
+        } else if (!bricks.subList(i + 1, bricks.size).any { it.canFall }) {
             result1++
         } else {
-            val fallen = ArrayList(bricks)
-            for (j in (i+1)..<fallen.size) {
-                if (fallen[j].canFall(fallen)) {
+            val fall = ArrayList(bricks)
+            for (j in (i + 1)..<fall.size) {
+                if (fall[j].canFall(fall)) {
                     result2++
-                    while (fallen[j].canFall(fallen)) fallen[j] = fallen[j].fallen
+                    while (fall[j].canFall(fall)) fall[j] = fall[j].fall
                 }
             }
         }
