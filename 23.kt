b2a2c515f743c88c8bf54db5f787da597fd869cb
pin2t <pin2t@mail.ru>
2023-12-23 16:34:03 +0400

    day 23

--- 23.kt
+++ 23.kt
@@ -3,20 +3,21 @@ import kotlin.math.max
 class Day23 {
     val up = Pair(0, -1); val right = Pair(1, 0); val down = Pair(0, 1); val left = Pair(-1, 0)
     val grid = ArrayList<String>()
-    val processed = HashSet<Pair<Int, Int>>()
+    var width = 0
+    var processed: BooleanArray = BooleanArray(0)
 
     inline fun move(pos: Pair<Int, Int>, dir: Pair<Int, Int>): Pair<Int, Int> = Pair(pos.first + dir.first, pos.second + dir.second)
 
     fun longest(from: Pair<Int, Int>, to: Pair<Int, Int>, steps: Int): Int {
         if (from == to) return steps
-        processed.add(from)
+        processed[from.second * width + from.first] = true
         var l = 0
         fun tryMove(dir: Pair<Int, Int>) {
             val next = move(from, dir);
             if (next.first >= 0 && next.second >= 0 &&
                 next.first < grid[0].length && next.second < grid.size &&
                 grid[next.second][next.first] != '#' &&
-                !processed.contains(next)) {
+                !processed[next.second * width + next.first]) {
                 l = max(l, longest(next, to, steps + 1))
             }
         }
@@ -25,7 +26,7 @@ class Day23 {
             '.' -> { tryMove(right); tryMove(down); tryMove(left); tryMove(up); }
             else -> throw IllegalStateException()
         }
-        processed.remove(from)
+        processed[from.second * width + from.first] = false
         return l
     }
 
@@ -33,10 +34,7 @@ class Day23 {
 
     inline fun tryMove2(from: Pair<Int, Int>, dir: Pair<Int, Int>, to: Pair<Int, Int>, steps: Int) {
         val next = move(from, dir);
-        if (next.first >= 0 && next.second >= 0 &&
-            next.first < grid[0].length && next.second < grid.size &&
-            grid[next.second][next.first] != '#' &&
-            !processed.contains(next)) {
+        if (grid[next.second][next.first] != '#' && !processed[next.second * width + next.first]) {
             longest2(next, to, steps + 1)
         }
     }
@@ -46,12 +44,12 @@ class Day23 {
             result2 = max(result2, steps)
             return
         }
-        processed.add(from)
+        processed[from.second * width + from.first] = true
         tryMove2(from, right, to, steps);
         tryMove2(from, down, to, steps);
         tryMove2(from, left, to, steps);
         tryMove2(from, up, to, steps)
-        processed.remove(from)
+        processed[from.second * width + from.first] = false
     }
 
     fun run() {
@@ -59,11 +57,14 @@ class Day23 {
             val line = readlnOrNull() ?: break
             grid.add(line)
         }
+        width = grid[0].length
+        processed = BooleanArray(grid.size * width) { false }
         val start = Pair(1, 0); val end = Pair(grid[0].length - 2, grid.size - 1)
         val result1 = longest(start, end, 0);
-        processed.clear()
         println(result1)
-        longest2(start, end, 0)
+        processed = BooleanArray(grid.size * width) { false }
+        processed[start.second * width + start.first] = true
+        longest2(Pair(1, 1), end, 1)
         println(listOf(result1, result2))
     }
 }
