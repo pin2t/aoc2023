c30fb6d333c7ae82f7291bbbbb8fe5c0d9a5fd25
pin2t <pin2t@mail.ru>
2024-01-07 09:09:17 +0400

    fix

--- 13.go
+++ 13.go
@@ -7,12 +7,14 @@ import "fmt"
 func mini(a int, b int) int { if a < b { return a }; return b }
 
 func horizontalReflection(grid [][]bool, other int) int {
-	for i := 0; i < len(grid); i++ {
+	for i := 0; i < len(grid) - 1; i++ {
 		var height = mini(i + 1, len(grid) - i - 1)
 		var reflected = true
 		for c := 0; (c < len(grid[0])) && reflected; c++ {
 			for j := 1; (j <= height) && reflected; j++ {
-				if grid[i + 1 - j][c] != grid[i + j][c] { reflected = false }
+				if grid[i + 1 - j][c] != grid[i + j][c] {
+					reflected = false
+				}
 			}
 		}
 		if reflected && (i + 1) != other { return i + 1 }
@@ -26,7 +28,7 @@ func reflection(grid [][]bool, other int) int {
 		var flipped [][]bool
 		for c := 0; c < len(grid[0]); c++ {
 			var row []bool
-			for r := 0; r < len(grid); r ++ { row = append(row, grid[r][c]) }
+			for r := 0; r < len(grid); r++ { row = append(row, grid[r][c]) }
 			flipped = append(flipped, row)
 		}
 		res = horizontalReflection(flipped, other % 100)
@@ -37,19 +39,24 @@ func reflection(grid [][]bool, other int) int {
 func main() {
 	var scanner = bufio.NewScanner(os.Stdin)
 	var grid [][]bool
-	var result, result2 = 0, 0
+	var part1, part2 = 0, 0
+	var process = func() {
+		var refl = reflection(grid, 0)
+		var other = 0
+		for r := 0; r < len(grid) && other == 0; r++ {
+			for c := 0; c < len(grid[0]) && other == 0; c++ {
+				grid[r][c] = !grid[r][c]
+				other = reflection(grid, refl)
+				grid[r][c] = !grid[r][c]
+			}
+		}
+		part1 += refl
+		part2 += other
+	}
 	for scanner.Scan() {
 		var line = scanner.Text()
 		if line == "" {
-			var refl = reflection(grid, 0)
-			var other = 0
-			for r := 0; r < len(grid) && other == 0; r ++ {
-				for c := 0; c < len(grid[0]) && other == 0; c++ {
-					grid[r][c] = !grid[r][c]
-				    other = reflection(grid, refl)
-				}
-			}
-			result += refl; result2 += other
+			process()
 			grid = [][]bool{}
 			continue
 		}
@@ -57,5 +64,6 @@ func main() {
 		for _, c := range line { row = append(row, c == '#') }
 		grid = append(grid, row)
 	}
-	fmt.Println(result, result2)
+	process()
+	fmt.Println(part1, part2)
 }
